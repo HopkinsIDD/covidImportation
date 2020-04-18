0eecbb59a366932a840f801906f37384cf193889
jkamins7 <jkaminsky@jhu.edu>
2020-04-18 18:36:43 -0400

    Updated to include some basic tests

---
+++ tests/testthat/test-example.R
@@ -0,0 +1,15 @@
+test_that("Test Cases work",{
+  expect_equal({
+    1+1
+  }, 2
+  )
+
+  expect_error({
+    stop("This is an error")
+  })
+
+  expect_error({
+    message("This is not an error")
+  }, NA)
+
+})
